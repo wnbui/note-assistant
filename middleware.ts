b05afdd6a1fdc9b375bedd72dbbd2623b321aead
wnbui <wnbui05@gmail.com>
2025-03-16 23:28:47 -0700

    Updated middleware to inclulde additional routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-    matcher: ['/protected1'], // Add more protected routes here
+    matcher: ['/protected1', '/recorder'], // Add more protected routes here
 };
